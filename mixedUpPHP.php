ef59ec4654e9af2bf7a2a2186e5ae74b9a8ed36f
kdragos <kristindragos@gmail.com>
2015-09-16 20:10:21 -0700

    Added php mixed up file.

---
+++ mixedUpPHP.php
@@ -0,0 +1,34 @@
+<?php 
+$selection = $_get["color"];
+
+$starter == "You've chosen ";
+
+if (selection = "1") {
+$item_message = "Red.";
+} else (selection == "2") {
+$item_message = "Blue.";
+} else if (selection = "3") {
+$item_message = "Green."
+} else {
+$item_message = "a color that doesn't exist.";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Color Selection</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<main>
+<div>
+<p>Your selection:</p>
+<h1><?php echo $starter . $item_message; ?>	
+</div>
+</h1>
+</main>
+</body>
+</html>
